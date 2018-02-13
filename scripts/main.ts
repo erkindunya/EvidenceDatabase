b07d3c5478beb12560b5d42977284dc051632cca
Christian Miles <Christian.Miles@kier.co.uk>
2018-02-13 17:09:46 +0000

    Add alert examples

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,13 +1,17 @@
 import 'airbnb-browser-shims';
 import { sp, Web } from './lib/sp';
 import * as $ from 'jquery';
+import sweetalert2 from 'sweetalert2';
+import * as toastr from 'toastr';
+
+const swal = sweetalert2;
 
 const web = new Web('https://uat-ext.kier.co.uk/sites/projects');
 
 web.lists.getByTitle('projects').items
   .select('*', 'Author/Title').expand('Author').get()
   .then((results: any[]) => {
-  // results go here
+    // results go here
     console.log(results);
   })
   .catch((error) => {
@@ -16,3 +20,7 @@ web.lists.getByTitle('projects').items
   });
 
 $('.js-red-text').css('color', 'red');
+
+swal('Any fool can use a computer');
+
+toastr.info('Hello');
