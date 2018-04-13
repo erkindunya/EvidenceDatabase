af06a2136b38ada0ba14cb084b087e60844f514f
Gabor Vig <Gabor.vig@kier.co.uk>
2018-04-13 23:43:53 +0100

    Testing List connection

--- scripts/export.ts
+++ scripts/export.ts
@@ -3,6 +3,26 @@ import { sp, Web } from './lib/sp';
 import * as $ from 'jquery';
 import * as toastr from 'toastr';
 
-export function exportToWord(curItemId:any, curItemTitle:any, curPath:any) {
-  alert('Hello World');
+export async function exportToWord(curItemId:any, curItemTitle:any, curPath:any) {
+  sp.web.select('Title').get().then((w) => {
+    alert(w.Title);
+    console.log(w.Title);
+  });
+  const urlSub = 'https://uat-ext.kier.co.uk/sites/projects';
+  const result: string = await getSiteTitle(urlSub);
+  console.log(result);
+}
+const web = new Web('https://uat-ext.kier.co.uk/sites/projects');
+
+let sites: any[] = null;
+
+async function getSiteTitle(url: string): Promise<string> {
+  if (sites == null) {
+    sites = await web.webs.select('title', 'ServerRelativeUrl').top(30000).get();
+  }
+  const result = sites.filter((value) => {
+    return value.ServerRelativeUrl === url;
+  });
+  if (result.length > 0) return result[0].Title;
+  return null;
 }
