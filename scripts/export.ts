d5daef2c6962d1895e5b6cf79cfd9fd987f3fc49
Gabor Vig <Gabor.vig@kier.co.uk>
2018-04-16 13:12:22 +0100

    Addressed issues from reivew meeting

--- scripts/export.ts
+++ scripts/export.ts
@@ -1,28 +1,77 @@
-import 'airbnb-browser-shims';
 import { sp, Web } from './lib/sp';
 import * as $ from 'jquery';
-import * as toastr from 'toastr';
 
-export async function exportToWord(curItemId:any, curItemTitle:any, curPath:any) {
-  sp.web.select('Title').get().then((w) => {
-    alert(w.Title);
-    console.log(w.Title);
-  });
-  const urlSub = 'https://uat-ext.kier.co.uk/sites/projects';
-  const result: string = await getSiteTitle(urlSub);
-  console.log(result);
+const web = sp.web;
+export async function exportToWord(curItemId:string, curItemTitle:string, curPath:string) {
+  const listName = curPath.substring(curPath.lastIndexOf('/Lists/') + 7, curPath.lastIndexOf('/'));
+  const listID = +getParameterByName('ID', curPath);
+  const listItem = await getListItems(listName,listID);
+  const fields = await getFields(listName);
+  const htmlData = populateTableforWord(listItem,fields);
+  
+  exportElementToWord(htmlData);
 }
-const web = new Web('https://uat-ext.kier.co.uk/sites/projects');
 
-let sites: any[] = null;
+function populateTableforWord(listItem:any,fields:any):string {
+  $('<h2 align="left">' + listItem.Title + '</h2>').appendTo('#mainExportContainer');
+  const $table = $('<table></table>');
+  $table.attr('id', listItem.ID);
+  $('<thead><tr><td><b>Column Name</b></td><td><b> \
+  Column Value</b></td><tr></thead>').appendTo($table);
 
-async function getSiteTitle(url: string): Promise<string> {
-  if (sites == null) {
-    sites = await web.webs.select('title', 'ServerRelativeUrl').top(30000).get();
-  }
-  const result = sites.filter((value) => {
-    return value.ServerRelativeUrl === url;
+  fields.forEach((field, index) => {
+    let row;
+    let rowData;
+    // Add field name row
+    row = $('<tr></tr>');
+    rowData = $('<td></td>').addClass('fieldName').text(field.Title);
+    row.append(rowData);
+    // Add field value row
+    rowData = $('<td></td>').addClass('fieldValue').text(listItem[field.InternalName]);
+    row.append(rowData);
+    $table.append(row);
   });
-  if (result.length > 0) return result[0].Title;
-  return null;
+  // Add table to the DIV
+  $table.appendTo($('#mainExportContainer'));
+  // $('#mainExportContainer table tr td:first-child').css('background-color', 'grey');
+  const htmlData = '<html xmlns:office="urn:schemas-microsoft-com:office:office" \
+  xmlns:word="urn:schemas-microsoft-com:office:word" xmlns="http://www.w3.org/TR/REC-html40"> \
+  <head><xml><word:WordDocument><word:View>Print</word:View> \
+  <word:Zoom>90</word:Zoom><word:DoNotOptimizeForBrowser/></word:WordDocument></head> \
+  <body>' + document.getElementById('mainExportContainer').innerHTML + '</body></html>';
+  return htmlData;
+}
+
+function getParameterByName(name, url) {
+  if (!url) url === window.location.href;
+  const nameTrimmed = name.replace(/[\[\]]/g, '\\$&');
+  const regex = new RegExp(`[?&]${nameTrimmed}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+async function getListItems(listName: string, listID: number): Promise<any> {
+  return web.lists.getByTitle(listName).items.getById(listID).get();
+}
+
+async function getFields(listName: string): Promise<any> {
+  return web.lists.getByTitle(listName).fields.filter('Hidden eq false').get();
+}
+
+function exportElementToWord(html) {
+  if (navigator.appName === 'Microsoft Internet Explorer') {
+    let iframe = document.getElementById('htmlDownloadFrame') as any;
+    iframe = iframe.contentWindow || iframe.contentDocument;
+    iframe.document.open('text/html', 'replace');
+    iframe.document.write(html);
+    iframe.document.close();
+    iframe.focus();
+    iframe.document.execCommand('SaveAs', true, 'Word.doc');
+  } else {
+    if (console && console.log) {
+      console.log('Please use an IE Browser.');
+    }
+  }
 }
