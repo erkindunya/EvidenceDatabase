a816d1749aa7db29242f0f95d571c4d0d8398ce3
Dolunas <gabor.vig@kier.co.uk>
2019-01-16 15:04:49 +0000

    A duplicate field name "WF_x0020_Status" was found. - Resolved by id Cha

--- AddNewFields.1.ps1
+++ AddNewFields.1.ps1
@@ -1,16 +1,3 @@
-# Get-PnPProvisioningTemplate -Out template.xml
-
-# Connect-PnPOnline –Url $siteUrl –Credentials (Get-Credential)
-#     Get-PnPProvisioningTemplate -Out $templatexml
-
-
-# $templatePath = Join-Path (Get-Location) '\resources\Fields.xml'
-Connect-PnPOnline -Url "https://28.kier.group" –Credentials (Get-Credential)
-# Get-PnPProvisioningTemplate -Out template.xml
-#Write-Output "test"
-#Get-PnPProvisioningTemplate -Out template.pnp -ContentTypeGroups "GConstruction EvidenceDB"
-#Write-Output "test"
-# Apply-PnPProvisioningTemplate -path $templatePath -overwrite
-# Add-PnPListItem -List "New Starter Submissions" -ContentType "NewStarterSubmissions" -Values @{"Title" = "Test Title"}Connect-PnPOnline -Url "https://28.kier.group" –Credentials (Get-Credential)
-
-Write-Output "test"
+$templatePath = Join-Path (Get-Location) '\resources\provtemplateUAtTrimmed.xml'
+Connect-PnPOnline -Url https://28.kier.group -CurrentCredentials
+Apply-PnPProvisioningTemplate -path $templatePath -overwrite
